ac46effc3fab5ed156978cac33ff943bbef2d0cb
DongjunLee <ehdwns2045@gmail.com>
2016-05-03 15:28:29 +0900

    Edit py_model neural_network

--- py_model/neural_network.py
+++ py_model/neural_network.py
@@ -10,7 +10,6 @@ class NeuralNetwork(object):
 		
 		self.l2 = {}
 		self.l2['layer'] = l2_layer['layer']
-#		self.l2['activation'] = l2_layer['activation']
 		self.__init_weight(self.l2)
 
 		self.loss_history = []
@@ -24,7 +23,7 @@ class NeuralNetwork(object):
 		loss = 0.0
 		num_train = X.shape[0]
 
-		_, _, output = self.forward(X)
+		_, output = self.forward(X)
 		correct_scores = output[np.arange(num_train), y]
 		loss = -np.sum(np.log(correct_scores))
 		
@@ -42,24 +41,24 @@ class NeuralNetwork(object):
 		l2_pre_act = l1_act.dot(self.l2['W']) + self.l2['b']
 		output = np.exp(l2_pre_act) / np.sum(np.exp(l2_pre_act), axis=1).reshape(num_train, 1)
 		
-		return l1_act, l2_pre_act, output
+		return l1_act, output
 
 	def backward(self, X, y, learning_rate, reg):
 
 		num_train = X.shape[0]
 		grads = {}
 
-		L1_score, _, output = self.forward(X)
+		l1_act, output = self.forward(X)
 		output[(np.arange(num_train), y)] = output[np.arange(num_train), y] - 1
+		output /= num_train
 
-		self.l2['W'] -= learning_rate * ( (L1_score.T).dot(output) + reg * (self.l2['W']) )
-		self.l2['b'] -= learning_rate * np.sum(output/num_train, axis=0)
+		self.l2['W'] -= learning_rate * ( (l1_act.T).dot(output) + reg * (self.l2['W']) )
+		self.l2['b'] -= learning_rate * np.sum(output, axis=0)
 					
-		dJdL1_score = (output/num_train).dot(self.l2['W'].T)
-		dJdL1_score[L1_score == 0] = 0
+		dJdL1_act = output.dot(self.l2['W'].T)
 			
-		self.l1['W'] -= learning_rate * ( (X.T).dot(dJdL1_score) + reg * (self.l1['W']) ) 
-		self.l1['b'] -= learning_rate * np.sum(dJdL1_score, axis=0)
+		self.l1['W'] -= learning_rate * ( (X.T).dot(dJdL1_act) + reg * (self.l1['W']) ) 
+		self.l1['b'] -= learning_rate * np.sum(dJdL1_act, axis=0)
 
 	def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=1000, batch_size=50, verbose=False):
 
